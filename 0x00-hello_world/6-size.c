00573b4d4e0e955da63a15c52cd9365835c863a8
Presidentsan <afeezsanusi32@gmail.com>
2023-09-07 16:02:51 -0700

    cisfun

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this describes the main function of the code
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of a char:: %lu byte(s)\n", (unsigned long)sizeof(a));
+	Printf("size of an int:%lu byte(s)\n", (unsigned long)sizeof(b));
+	Printf("size of a long int:%lu byte(s)\n", (unsigned long)sizeof(c));
+	Printf("size of a long long int:lu byte(s)\n", (unsigned long)sizeof(d));
+	Printf("size of a float int:lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
+}
